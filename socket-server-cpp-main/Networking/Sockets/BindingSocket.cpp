856d2036fde232d82fe30400c2a7685d8753d1e4
can <canavci2016@gmail.com>
2022-08-29 13:21:03 +0100

    redundant logs were removed from the code

--- socket-server-cpp-main/Networking/Sockets/BindingSocket.cpp
+++ socket-server-cpp-main/Networking/Sockets/BindingSocket.cpp
@@ -4,7 +4,6 @@
 // Constructor
 HDE::BindingSocket::BindingSocket(int domain, int service, int protocol, int port, u_long interface) : SimpleSocket(domain, service, protocol, port, interface)
 {
-    std::cout << "bind_ops was called from BindingSocket constructor" << std::endl;
     set_connection(connect_to_network(get_sock(), get_address()));
     test_connection(get_connection());
 }
